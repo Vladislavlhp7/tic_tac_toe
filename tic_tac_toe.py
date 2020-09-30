828d9097953455ebe00999a0ca956d41838f37cb
Vladislav Yotkov <vladislav.yotkov7@gmail.com>
2020-09-30 14:39:41 +0300

    Player1 choses tiles

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -9,5 +9,35 @@ def print_board(board):
 			print('|', end="")
 		line += 1
 
+def tile_free(board, num):
+	if board[num] == '-':
+		return True
+	return False
+
+# Some variables
 board = ["-"]*9
+num = -1
+str_in = " "
+turn = 0
+game_not_won = True
+
 print_board(board)
+
+# Start game
+while(game_not_won):
+	while(turn == 0):
+		try:
+			num = int(input("Choose a tile [from 0 to 8]:"))
+		except ValueError:
+			print(str_in)
+			while(len(str_in) > 1 or ord(str_in) < 48 or ord(str_in) > 57):
+				str_in = input("Choose a valid tile [from 0 to 8]:")
+			num = int(str_in)
+
+		if num >= 0 and num <= 8:
+			if tile_free(board, num):
+				board[num] = 'x'
+				turn = 1
+		print_board(board)
+	while(turn == 1):
+		break;
