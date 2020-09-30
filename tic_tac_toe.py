7f6ab99cd41d8f116a3ae5ecd2c503e20b3def03
Vladislav Yotkov <vladislav.yotkov7@gmail.com>
2020-09-30 21:11:30 +0300

    evaluation

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,3 +1,18 @@
+import numpy as np
+
+class Turn:
+		
+	def __init__(self, id):
+		self.id = id
+		self.value = 0
+		self.next = [None]*8
+
+def deleteTurn(turn):
+	if turn is not None:
+		for i in range(8):
+			deleteTurn(turn.next[i])
+			turn.next[i] = None
+
 def print_board(board):
 	line = 0
 	for elem in board:
@@ -14,6 +29,59 @@ def tile_free(board, num):
 		return True
 	return False
 
+def points(board, player):
+	value = 0
+	sum_rows = 0
+	sum_cols = 0
+	sum_diagonal = 0
+	matrix = [[0 for i in range(3)] for j in range(3)]
+
+	for i, tile in enumerate(board):
+		temp = []
+		if(tile == 'x'):
+			matrix[int(i/3)][i%3] = 1
+		elif(tile == 'o'):
+			matrix[int(i/3)][i%3] = -1
+	print(matrix)
+
+	# Check if 3 same tiles are on the same row
+	for row in matrix:
+		sum_rows = sum(row)
+		if(sum_rows == 3 or sum_rows == -3):
+			value = 10
+			if player == 1:
+				return value
+			else:
+				return value*(-1)
+
+	# Check if 3 same tiles are on the same col
+	for j in range(3):
+		sum_cols = 0
+		for i in range(3):
+			sum_cols += matrix[i][j]
+		if(sum_cols == 3 or sum_cols == -3):
+			value = 10
+			if player == 1:
+				return value
+			else:
+				return value*(-1)
+	# Check if 3 same tiles are on the same diagonal
+	sum_diagonal = matrix[0][0]+matrix[1][1]+matrix[2][2]
+	if(sum_diagonal == 3 or sum_diagonal == -3):
+		value = 10
+		if player == 1:
+			return value
+		else:
+			return value*(-1)
+	# Check if 3 same tiles are on the same diagonal
+	sum_diagonal = matrix[2][0]+matrix[1][1]+matrix[0][2]
+	if(sum_diagonal == 3 or sum_diagonal == -3):
+		value = 10
+		if player == 1:
+			return value
+		else:
+			return value*(-1)
+
 # Some variables
 board = ["-"]*9
 num = -1
@@ -22,6 +90,9 @@ turn = 0
 game_not_won = True
 
 print_board(board)
+
+# Let it be that the standard player uses 'x'
+# He is also represented with the boolean value of 1
 
 # Start game
 while(game_not_won):
@@ -39,5 +110,9 @@ while(game_not_won):
 				board[num] = 'x'
 				turn = 1
 		print_board(board)
+		print(points(board, 1))
+
 	while(turn == 1):
+		turn = 0
 		break;
+	# points(board_to_matrix(board), 1)
